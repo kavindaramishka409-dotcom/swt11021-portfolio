4f5615036e9a10d09748b368cdc13b978f6931cb
kavindaramishka409-dotcom <kavindaramishka409@gmail.com>
2026-05-11 15:46:31 +0530

    Week 1: hello world, pipeline demo, greet, escape

---
+++ week1/escape.c
@@ -0,0 +1,12 @@
+  #include <stdio.h>
+
+int main(void) {
+    // B4: Escape Sequences
+    printf("Name:\tAbdul Haleem\n");
+    printf("Course:\tSWT 11012\n");
+    printf("\n");  // blank line
+    printf("Path:\tC:\\Users\\student\n");
+    printf("He said: \"Hello!\"\n");
+    
+    return 0;
+}
---
+++ week1/greet.c
@@ -0,0 +1,10 @@
+ #include <stdio.h> 
+  
+int main(void) { 
+     printf("K.K.Kavinda ramishka perera \n");
+     printf("SEU/IS/24/ICT/017\n");
+     printf("year 1\n");
+
+  
+    return 0; 
+} 
